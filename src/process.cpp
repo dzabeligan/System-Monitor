a1bb42d562486a147e58172e085d65e027eddc54
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-08-25 20:04:06 +0100

    feat: Implement Process Object

--- src/process.cpp
+++ src/process.cpp
@@ -1,33 +1,70 @@
+#include "process.h"
+
 #include <unistd.h>
+
 #include <cctype>
 #include <sstream>
 #include <string>
 #include <vector>
 
-#include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+/**
+ * @brief Returns this process's ID
+ *
+ * @return int
+ */
+int Process::Pid() const { return pid_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+/**
+ * @brief Returns this process's CPU utilization
+ *
+ * @return float
+ */
+float Process::CpuUtilization() {
+  return static_cast<float>(LinuxParser::ActiveJiffies(Pid())) /
+         LinuxParser::Jiffies();
+}
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+/**
+ * @brief Returns the command that generated this process
+ *
+ * @return string
+ */
+string Process::Command() const { return LinuxParser::Command(Pid()); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+/**
+ * @brief Returns this process's memory utilization
+ *
+ * @return string
+ */
+string Process::Ram() const { return LinuxParser::Ram(Pid()); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+/**
+ * @brief Returns the user (name) that generated this process
+ *
+ * @return string
+ */
+string Process::User() const { return LinuxParser::User(Pid()); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+/**
+ * @brief Returns the age of this process (in seconds)
+ *
+ * @return long int
+ */
+long int Process::UpTime() const { return LinuxParser::UpTime(Pid()); }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+/**
+ * @brief Overloads the "less than" comparison operator for Process objects
+ *
+ * @param a
+ * @return true
+ * @return false
+ */
+bool Process::operator<(Process const& a) const {
+  return std::stol(Ram()) < std::stol(a.Ram());
+}
