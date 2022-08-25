2eb3d438834c6265ad187a3a87b4880f5233af03
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-08-25 20:04:47 +0100

    feat: Implement Processor Object

--- include/processor.h
+++ include/processor.h
@@ -3,10 +3,11 @@
 
 class Processor {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
+  float Utilization();
 
-  // TODO: Declare any necessary private members
  private:
+  unsigned long activeTime_{0};
+  unsigned long idleTime_{0};
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,25 @@
 #include "processor.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include <numeric>
+#include <vector>
+
+#include "linux_parser.h"
+
+/**
+ * @brief Returns the aggregate CPU utilization
+ *
+ * @return float
+ */
+float Processor::Utilization() {
+  long idleTime = LinuxParser::IdleJiffies();
+  long activeTime = LinuxParser::ActiveJiffies();
+  long dIdleTime = idleTime - idleTime_;
+  long dNonIdleTime = activeTime - activeTime_;
+
+  float percentage =
+      static_cast<float>(dNonIdleTime) / (dIdleTime + dNonIdleTime);
+  idleTime_ = idleTime;
+  activeTime_ = activeTime;
+
+  return percentage;
+}
