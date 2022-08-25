0b36efa3665565730f73fc594cb059d81fdfe28a
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-08-25 20:02:16 +0100

    feat: Implement format time

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,25 @@
 #include <string>
 
-#include "format.h"
+#include "../include/format.h"
 
 using std::string;
+using std::to_string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+static inline string normalizeTimeToken(long time) {
+  return time < 10 ? (string("0") + to_string(time)) : to_string(time);
+}
+
+/**
+ * @brief takes Long int measuring seconds and output time in 'HH:MM:SS'
+ *
+ * @param seconds 
+ * @return string 
+ */
+string Format::ElapsedTime(long seconds) {
+  long hours = seconds / (60 * 60);
+  seconds %= (60 * 60);
+  long minutes = seconds / 60;
+  seconds %= 60;
+  return normalizeTimeToken(hours % 24) + string(":") + normalizeTimeToken(minutes) +
+         ":" + normalizeTimeToken(seconds);
+}
